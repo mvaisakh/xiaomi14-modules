6282e5b9f196603f9d070dcc536193d436253439
Vaisakh Murali <mvaisakh@statixos.com>
2026-04-11 00:06:58 +0530

    touch: add houji target definition
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

---
+++ qcom/opensource/touch-drivers/config/gki_houjitouch.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2022, Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
+#define CONFIG_QTS_ENABLE 1
+#define CONFIG_TOUCHSCREEN_SYNA_TCM2 1
+#define CONFIG_TOUCHSCREEN_XIAOMI_TOUCHFEATURE 1
